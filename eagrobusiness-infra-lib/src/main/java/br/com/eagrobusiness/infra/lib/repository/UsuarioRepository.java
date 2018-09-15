43137694c42fb878c52297b72a1bc996e8a59ca8
Edu Gonçalves <es_goncalves@msn.com>
2018-09-15 01:05:21 -0300

    Retirada das targets

--- eagrobusiness-infra-lib/src/main/java/br/com/eagrobusiness/infra/lib/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.eagrobusiness.infra.lib.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import br.com.eagrobusiness.infra.lib.model.usuario.Usuario;
-
-public interface UsuarioRepository extends CrudRepository<Usuario, String>{
-	Usuario findByUsername(String userName);
-}
--- eagrobusiness-lib/src/main/java/br/com/eagrobusiness/lib/infra/repository/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.eagrobusiness.lib.infra.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UsuarioRepository extends CrudRepository<Usuario, String>{
-
-}
